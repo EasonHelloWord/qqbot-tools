c81aa9aad57fddddf9bc0d3556c6574cc6ed6928
EasonJan <a13564674280@163.com>
2023-09-29 14:37:39 +0800

    【新增】程序模块化 【新增】主函数发信和发报区分 【新增】复读机模块 【修改】忽略同步.vscode文件夹

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import requests
 from flask import Flask, request
+from apps import repeat
 app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def post_data():
     data = request.get_json()
     if data.get('post_type') == 'message':
-        print(data)
         receive(data)
     return ('200')
+
 def receive(data):
     if data.get('message')[0] == ".":
         data['message'] = data.get('message')[1:]
-def send(URL, data):
-    print('send')
+        print(data)
+        message = repeat.main(data.get('message'))
+        message_type = data.get('message_type')
+        user_id = data.get('user_id',None)
+        group_id = data.get('group_id',None)
+        send_message(message_type,message,user_id,group_id)
+
+
+def send_message(message_type,message,user_id=None,group_id=None,auto_escape=False):
+    data = {'message_type':message_type,
+            'user_id':user_id,
+            'group_id':group_id,
+            'message':message,
+            'auto_escape':auto_escape}
+    requests.post(f"http://127.0.0.1:5700/send_msg", data)
+
+def post(URL, data):
     requests.post(f"http://127.0.0.1:5700/{URL}", data)
 if __name__ == '__main__':
     app.run('127.0.0.1', 5701, True)

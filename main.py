8959e060d641163596fb3ffb65d03e224bc8907c
EasonJan <a13564674280@163.com>
2023-09-29 14:51:23 +0800

    【修改】代码格式优化，便于扩展程序

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ def post_data():
     return ('200')
 
 def receive(data):
-    if data.get('message')[0] == ".":
+    if data.get('message_type') == 'group' and data.get('message')[0] == ".":
         data['message'] = data.get('message')[1:]
-        print(data)
-        message = repeat.main(data.get('message'))
-        message_type = data.get('message_type')
-        user_id = data.get('user_id',None)
-        group_id = data.get('group_id',None)
-        send_message(message_type,message,user_id,group_id)
+        tools(data)
+    if data.get('message_type') == 'private':
+        tools(data)
 
+def tools(data):
+    repeat_(data)
+
+def repeat_(data):
+    message = repeat.main(data.get('message'))
+    message_type = data.get('message_type')
+    user_id = data.get('user_id',None)
+    group_id = data.get('group_id',None)
+    send_message(message_type,message,user_id,group_id)
 
 def send_message(message_type,message,user_id=None,group_id=None,auto_escape=False):
     data = {'message_type':message_type,
@@ -30,5 +36,6 @@ def send_message(message_type,message,user_id=None,group_id=None,auto_escape=Fal
 
 def post(URL, data):
     requests.post(f"http://127.0.0.1:5700/{URL}", data)
+
 if __name__ == '__main__':
     app.run('127.0.0.1', 5701, True)
